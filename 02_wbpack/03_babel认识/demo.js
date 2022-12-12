4162c73e9ea2c975a184d96bce940403e968d788
LikeK423 <88269600+LikeK423@users.noreply.github.com>
2022-12-12 19:08:57 +0800

    null

---
+++ 02_wbpack/03_babel认识/demo.js
@@ -0,0 +1,20 @@
+// 开发中，要想使用ES6的语法、TS和开发React项目都离不开Babel
+// babel本身可以作为一个独立工具，不和webpack等构建工具配置来单独使用（需先安装@babel/core 和 @babel/cli）
+
+// 1. 示例代码
+const names = ["桑昆","端木元","玉磬子"];
+names.forEach(item => console.log(item))
+
+// 2.运行 npx babel demo.js --out-dir dist 结果：与该文件一模一样
+
+// 3.先转换箭头函数,安装 npm i @babel/plugin-transform-arrow-functions
+
+// 4.运行 npx babel demo.js --out-dir dist --plugins=@babel/plugin-transform-arrow-functions 结果：箭头函数成功转化
+
+// 5.转换const和let,安装 npm i @babel/plugin-transform-block-scoping
+
+// 6.运行 npx babel demo.js --out-dir dist --plugins=@babel/plugin-transform-arrow-functions,@babel/plugin-transform-block-scoping 结果：const和let转换为var
+
+// 7.每个ES6语法安装对应插件过于麻烦，因此babel有preset（预设）,安装:npm i @babel/preset-env
+
+// 8.运行：npx babel demo.js --out-dir dist --presets=@babel/preset-env
